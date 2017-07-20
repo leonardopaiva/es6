11c859566f8655c8781d9512200f52509ecc8d91
Leonardo Souza Paiva <leonardo@Leonardos-MacBook-Pro.local>
2017-07-20 10:13:52 -0300

    ES6 classes my job

---
+++ index.js
@@ -0,0 +1,66 @@
+class Message {
+  constructor(text = '', created = Date.now()) {
+    this.text = text;
+    this.created = created;
+  }
+
+  get created() {
+    return this._created;
+  }
+
+  set created(created) {
+    if(!created || isNaN(created)) {
+      throw new Error('Invalid created');
+    }
+    this._created = created;
+  }
+
+  toString() {
+    return `Message created at: ${this.created} - Text: ${this.text}`;
+  }
+}
+
+
+class ImageMessage extends Message {
+  constructor(text = '', created = Date.now(),
+              url = '', thumbnail = '') {
+    super(text, created);
+    this.url = url;
+    this.thumbnail = thumbnail;
+  }
+  toString() {
+    return `Photo${super.toString()} ` +
+            `- Url: ${this.url} ` +
+            `Thumbnail: ${this.thumbnail}`;
+  }
+}
+
+var text = 'Some text';
+var created = Date.now();
+
+var duckTypeMessage = {
+  text,
+  created
+};
+//equivale a:
+// var duckTypeMessage = {
+//   text: text,
+//   created: text
+// }
+console.log(duckTypeMessage);
+
+var emptyMessage = new Message();
+var textMessage = new Message('Yesterday message', Date.now() - 86400);
+var photoMessage = new ImageMessage();
+
+console.log(emptyMessage);
+console.log(textMessage);
+console.log(photoMessage);
+
+console.log(emptyMessage instanceof Message);
+console.log(textMessage instanceof Message);
+console.log(photoMessage instanceof Message);
+console.log(photoMessage instanceof ImageMessage);
+
+console.log(emptyMessage instanceof ImageMessage);
+console.log(duckTypeMessage instanceof ImageMessage);
